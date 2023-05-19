0bd13544873c3356f2ea7d8a34a72020582ad58f
Avinash Goen <agoen97@gmail.com>
2023-05-19 19:33:38 +0530

    Fully Connected Neural Network Integration

--- dataset.py
+++ dataset.py
@@ -7,12 +7,22 @@ from sklearn.decomposition import PCA
 
 def get_raw():
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    return (x_train, y_train.ravel()), (x_test, y_test.ravel())
+    return (x_train, y_train), (x_test, y_test)
 
 def get_2D_normalised():
     (x_train, y_train), (x_test, y_test) = get_raw()
+    
     x_train = x_train.reshape(x_train.shape[0], -1) / 255.0
     x_test = x_test.reshape(x_test.shape[0], -1) / 255.0
+    
+    # Calculate the mean and standard deviation of the pixel values over the training dataset
+    mean = np.mean(x_train, axis=(0,1))
+    std = np.std(x_train, axis=(0,1))
+    
+    # Normalize the pixel values by subtracting the mean and dividing by the standard deviation
+    x_train = (x_train - mean) / std
+    x_test = (x_test - mean) / std
+    
     return (x_train, y_train), (x_test, y_test)
 
 def test_dimensionality_reduction(components):
