84173f5486299a3932e05d0c40924bc7015338c0
Avinash Goen <agoen97@gmail.com>
2023-05-25 22:42:17 +0530

    Remove Unwanted function

--- Utils/network.py
+++ Utils/network.py
@@ -8,13 +8,3 @@ def first_class(iterable, element, default=None):
         return next(elements)
     except:
         return default
-
-
-def get_nearest_dense_layer(iterable, element, dense_class, default=None):
-    layers_till_element = []
-    for i in iterable:
-        if i == element:
-            break
-        layers_till_element.append(i)
-
-    return first_class(layers_till_element[::-1], dense_class, default)
